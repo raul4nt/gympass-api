a8aeb6a2f3e9b60146f01db6210de9acc38323c4
Raul Silveira <raulantsilveira@gmail.com>
2024-11-01 01:00:18 -0300

    feat: create unity tests using  in memory users repository

--- src/use-cases/register-use-case.ts
+++ src/use-cases/register-use-case.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository-interface'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -11,10 +12,18 @@ interface RegisterUseCaseRequest {
 // SOLID
 // D- Dependency Inversion Principle
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -25,11 +34,13 @@ export class RegisterUseCase {
 
     // const prismaUsersRepository = new PrismaUsersRepository()
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
 
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,70 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register-use-case'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    // espero que o id do usuário retornado por esta função seja
+    // qualquer string
+  })
+
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+    // o compare do bcrypt serve justamente pra comparar duas
+    // senhas, ajudando a descobrir se o hash esta funcionando mesmo
+    // vai retornar true ou false, dependendo do resultado da  comparaçao
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+    // promises ou dao resolve ou reject. resolve = deu certo, reject =
+    // deu erro. o vitest tem o rejects e o resolves, para podermos usar o
+    // expect em ambos casos. neste caso, eu espero que rejeite, e que a rejeiçao
+    // seja de uma instancia UserAlreadyExistsError, que é o erro que deveria dar
+    // quando o usuario ja existe(email repetido)
+    // ou seja, estamos buscando o retorno vindo através desta rejeição
+  })
 })
