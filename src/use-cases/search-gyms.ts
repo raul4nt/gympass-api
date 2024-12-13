0c733feeed82ea241b11009f89a5f1fea6445bee
Raul Silveira <raulantsilveira@gmail.com>
2024-12-13 13:57:16 -0300

    user is now enable to search gyms by name

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,27 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+  // query = busca nesse caso
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return {
+      gyms,
+    }
+  }
+}
